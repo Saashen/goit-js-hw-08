d2f4dbdb9e1e7d9f5cf54fe3488d90e0884e3613
Saashen <saashen.kyiv@gmail.com>
2019-10-19 13:35:23 +0300

    module 8 fixed 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
-import images from './gallery-items.js';
+import galleryItemsfrom from './gallery-items.js';
 
-const gallery = document.querySelector('.gallery');
-const lightBox = document.querySelector('.lightbox');
-const closeLightBoxBtn = document.querySelector(
-  'button[data-action="close-lightbox"]',
-);
-const lightBoxImage = lightBox.children[1].children[0];
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  lightbox: document.querySelector('.lightbox'),
+  lightboxContent: document.querySelector('.lightbox__content'),
+  lightboxImage: document.querySelector('.lightbox___image'),
+};
 
-const html = images
+const setGalleryHTML = arr => arr
   .map(
     ({ preview, original, description }) => `<li class="gallery__item">
 <a
@@ -20,6 +20,7 @@ const html = images
     data-source="${original}"
     alt="${description}"
   />
+
   <span class="gallery__icon">
     <i class="material-icons">zoom_out_map</i>
   </span>
@@ -28,60 +29,39 @@ const html = images
   )
   .join('\n');
 
-gallery.insertAdjacentHTML('afterbegin', html);
-gallery.addEventListener('click', handleImageClick);
+const selected = {
+  elem: null,
+};
 
 function handleImageClick(event) {
   event.preventDefault();
+  const { target } = event;
 
-  const target = event.target;
-  if (target.nodeName !== 'IMG') return;
-  setActiveLink(target);
+  if (selected.elem === target) {
+    refs.lightbox.classList.add('is-open');
+    selected.elem = target;
+    return;
+  }
+
+  const image = refs.lightboxImage.cloneNode(false);
+  image.src = target.dataset.source;
+  image.alt = target.alt;
+  refs.lightboxContent.innerHTML = '';
+  refs.lightboxContent.appendChild(image);
+
+  refs.lightbox.classList.add('is-open');
 }
 
-function setActiveLink(nextActiveLink) {
-  lightBoxImage.src = nextActiveLink.dataset.source;
-  lightBoxImage.alt = nextActiveLink.alt;
+function closelightbox(event) {
+  const { target } = event;
 
-  const currentActiveLink = gallery.querySelector('img.active');
-  const activeElement = { link: currentActiveLink };
-  const activeLink = activeElement.link;
-
-  //Минулий варіант гарний тим, що завантажується одразу потрібна картинка.
-  //
-  // const currentActiveLink = gallery.querySelector('img.active');
-  // if (currentActiveLink) {
-  //   currentActiveLink.classList.remove('active');
-  // }
-  // nextActiveLink.classList.add('active');
-
-  //А тут при повільному інтернеті або занадто швидкому перемиканню,
-  //спочатку "висить" стара картинка, а потім уже її замінює нова
-  if (activeLink === null) {
-    nextActiveLink.classList.add('active');
-  } else if (activeLink !== nextActiveLink) {
-    activeLink.classList.remove('active');
-    nextActiveLink.classList.add('active');
-  } else return;
-  console.log(nextActiveLink);
-
-  lightBox.classList.add('is-open');
-  window.addEventListener('keydown', closeLightBoxByEsc);
-  closeLightBoxBtn.addEventListener('click', closeLightBox);
-  lightBox.addEventListener('click', closeLightBoxModal);
+  if (event.type === 'keydown' && event.code !== 'Escape') return;
+  if (event.type === 'click' && target.nodeName === 'IMG') return;
+  refs.lightbox.classList.remove('is-open');
 }
 
-function closeLightBox() {
-  lightBox.classList.remove('is-open');
-  window.removeEventListener('keydown', closeLightBoxByEsc);
-}
+refs.gallery.insertAdjacentHTML('afterbegin', setGalleryHTML(galleryItemsfrom));
 
-function closeLightBoxModal(event) {
-  if (event.target !== lightBox.children[1]) return;
-  closeLightBox();
-}
-
-function closeLightBoxByEsc(event) {
-  if (event.code !== 'Escape') return;
-  closeLightBox();
-}
+refs.gallery.addEventListener('click', handleImageClick);
+refs.lightbox.addEventListener('click', closelightbox);
+window.addEventListener('keydown', closelightbox);
